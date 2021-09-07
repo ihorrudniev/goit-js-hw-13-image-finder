edb2d46e5f4fcedc280835e5dcc69cfdaea9e69a
Ihor Rudniev <rudnievihor@gmail.com>
2021-09-07 21:55:44 +0300

    add-files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import { getPictures } from './js/apiService';
+import { refs } from './js/refs.js';
+import templateCard from './templates/templateCard.hbs';
+import data from 'browsersList';
+import { alert, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import * as basicLightbox from 'basiclightbox';
+
+refs.form.addEventListener('submit', onSearch);
+refs.button.addEventListener('click', onLoadMore);
+refs.button.style.visibility = 'hidden';
+refs.gallery.addEventListener('click', onOpenModal);
+
+const initialData = {
+  page: 1,
+  query: '',
+};
+
+async function onSearch(e) {
+  try {
+    e.preventDefault();
+    refs.button.style.visibility = 'hidden';
+    initialData.page = 1;
+    initialData.query = e.currentTarget.elements.query.value.trim();
+
+    if (!initialData.query) {
+      return error({
+        text: 'Please, type your query!',
+      });
+    }
+
+    const response = await getPictures(initialData.query, initialData.page);
+    const result = await response.data.hits;
+    refs.gallery.innerHTML = templateCard(result);
+
+    if (result.length > 11) {
+      refs.button.style.visibility = 'visible';
+    } else if (result.length === 0) {
+      return alert({
+        text: 'No matches found! Please, try again!',
+      });
+    }
+  } catch (error) {
+    Error;
+  }
+}
+
+async function onLoadMore() {
+  try {
+    initialData.page += 1;
+    const response = await getPictures(initialData.query, initialData.page);
+    const result = await response.data.hits;
+    refs.gallery.insertAdjacentHTML('beforeend', templateCard(result));
+
+    if (result.length < 12) {
+      refs.button.style.visibility = 'hidden';
+    }
+    refs.gallery.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  } catch (error) {
+    Error;
+  }
+}
+
+function onOpenModal(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+<img src="${e.target.dataset.src}" width="800" height="600">
+`);
+
+  instance.show();
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,9 +1,7 @@
-function addPictures() {
-  return fetch(
-    'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=23243839-9e0dbc2f727b7d3ca19c2cc88',
-  )
-    .then(res => res.json())
-    .then(console.log());
-}
+import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+const key = '23243839-9e0dbc2f727b7d3ca19c2cc88';
 
-addPictures(flower);
+export function getPictures(query, page) {
+  return axios.get(`&q=${query}&page=${page}&per_page=12&key=${key}`);
+}
