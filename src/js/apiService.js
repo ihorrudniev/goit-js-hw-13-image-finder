1ee2658a0cc87c702a1af550cd5db379654636a8
Ihor Rudniev <rudnievihor@gmail.com>
2022-04-28 19:53:02 +0300

    hidden_key

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
-const key = '23243839-9e0dbc2f727b7d3ca19c2cc88';
+const KEY = process.env.KEY;
 
 export function getPictures(query, page) {
-  return axios.get(`&q=${query}&page=${page}&per_page=12&key=${key}`);
+  return axios.get(`&q=${query}&page=${page}&per_page=12&key=${KEY}`);
 }
